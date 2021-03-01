11db090cd1989c4a35e05bc544a61cbba47ec976
McCambley <jakemccamb16@gmail.com>
2021-02-28 23:26:57 -0500

    Learning props and state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    person: [
+      {name: 'Jake', age: 29},
+      {name: 'Eddie', age: 32},
+      {name: 'Holly', age: 24},
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <h2>This is really working!</h2>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <button>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}>I'm the author of this app</Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+        <Person name="Cam" age="24"/>
       </div>
     );
   }
