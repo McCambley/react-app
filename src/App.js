46063435d1e5f1486ba6638268110fbfa9fbee32
McCambley <jakemccamb16@gmail.com>
2021-03-03 13:29:47 -0500

    Working with Hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import person from './Person/Person';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    person: [
-      {name: 'Jake', age: 29},
-      {name: 'Eddie', age: 32},
-      {name: 'Holly', age: 24},
-    ]
-  }
+const app = props => {
+  const [personsState, setPersonsState] = useState(
+    {
+      person: [
+        {name: 'Jake', age: 29},
+        {name: 'Eddie', age: 32},
+        {name: 'Holly', age: 24},
+      ], 
+      otherState: 'some other value'
+    }
+  );
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <h2>This is really working!</h2>
-        <button>Switch Name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}>I'm the author of this app</Person>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
-        <Person name="Cam" age="24"/>
-      </div>
-    );
-  }
-}
+  console.log(personsState)
 
-export default App;
+  const switchNameHandler = () => {
+    // console.log('Was Clicked!');
+    // DONT DO THIS!!! this.state.person[0].name = 'George Jacob';
+    setPersonsState({
+      person: [
+        {name: 'George', age: 29},
+        {name: 'Edward', age: 26},
+        {name: 'Holly Chase', age: 26},
+      ],
+      otherState: personsState.otherState
+    })
+  } 
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <h2>This is really working!</h2>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.person[0].name} age={personsState.person[0].age}>I'm the author of this app</Person>
+      <Person name={personsState.person[1].name} age={personsState.person[1].age}/>
+      <Person name={personsState.person[2].name} age={personsState.person[2].age}/>
+    </div>
+  );
+};
+
+export default app;
+
+/* state = {
+  person: [
+    {name: 'Jake', age: 29},
+    {name: 'Eddie', age: 32},
+    {name: 'Holly', age: 24},
+  ]
+} */
+
+
